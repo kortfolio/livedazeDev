afb3bad69bec1cc817bc199f1fa0db7ef7471df1
Mark Kang <kangm210@gmail.com>
2019-02-14 21:13:53 -0800

    Developed basic UI and functionality for setting a goal date
    Still needs some UI Improvements and conditional rendering for calendar part.

---
+++ src/_components/GoalDate/BeforeSetGoalDate.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { AfterSetGoalDate } from "../GoalDate/AfterSetGoalDate";
+
+export class BeforeSetGoalDate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: new Date(),
+      isClicked: true
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(date) {
+    this.setState({
+      startDate: date
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    this.setState({
+      isClicked: false
+    })
+
+  }
+
+  render() {
+    let isClicked = this.state.isClicked;
+
+    return (
+      <React.Fragment>
+      {
+        (isClicked)
+          ? (
+
+          <form onSubmit={this.handleSubmit}>
+          <div className="SetGoalDatePlaceHolder">Set your D day</div>
+          <div className="centerStuff">
+          <label>
+            <DatePicker
+            selected={this.state.startDate}
+            onChange={this.handleChange}
+            />
+            </label>
+            <br/>
+            <button type="submit" className="waves-effect waves-light btn PrimaryBtnColor">Update</button>
+            </div>
+          </form>
+            )
+          : (
+
+                      <AfterSetGoalDate newDate={this.state.startDate}/>
+
+          )
+      }
+      </React.Fragment>
+
+
+    );
+  }
+}
