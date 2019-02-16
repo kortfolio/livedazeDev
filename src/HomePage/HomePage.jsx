3216744e2e8b4f3e659ce17047f7b47f0e9cf27c
Mark Kang <kangm210@gmail.com>
2019-02-15 19:06:00 -0800

    Removed Unused components

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -6,27 +6,16 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { userActions } from "../_actions";
 
-import Moment from "react-moment";
+import moment from "moment";
 import "moment-timezone";
 import "moment-duration-format";
-import moment from "moment";
 
 import { Button, Card, Row, Col, Icon } from "react-materialize";
 import M from "materialize-css";
 
-import { PomodoroTimer } from "../_components/pomodoroTimer";
-import { WakeUpTime } from "../_components/WakeUpTime";
-import { DailyToDo } from "../_components/DailyToDo";
-import {LoginControl} from '../_components/LoginControl';
-
-import { WorkEndTime } from '../_components/WorkEndTime';
-
-import { SinceWakeUp } from '../_components/SinceWakeUp';
-
-
-
-
-
+import { PomodoroTimer } from "../_components/PomodoroTimer/pomodoroTimer";
+import { SetGoalDateView } from "../_components/GoalDate/SetGoalDateView";
+import { PreWakeUpTime } from "../_components/WakeUpTime/PreWakeUpTime";
 
 class HomePage extends React.Component {
   componentDidMount() {
@@ -42,6 +31,7 @@ class HomePage extends React.Component {
     const rows = [];
 
     const { user, users } = this.props;
+
     const wakeUpTime = "2019-01-19T07:00-0800";
     const sleepTime = "2019-01-18T22:00-0800";
 
@@ -62,141 +52,53 @@ class HomePage extends React.Component {
     return (
       <div className="wrapper">
         <div className="livedaze_pre_headline">
-          View 0 : Default/First User Login
+          View 0 : Default/First User Login <br />
+          Date Picker : https://www.npmjs.com/package/react-datepicker
         </div>
-        <div className="row flex">
 
+        <div className="row flex">
           {/**Daily To do List **/}
-          <div className="col l4 s12">
-            <DailyToDo/>
-          </div>
+          <div className="col l4 s12" />
 
           {/**Wake Up Time **/}
           <div className="col l4 s12">
-              <WakeUpTime/>
-                      <LoginControl/>
+            <div className="card-panel livedazeGrey">
+              <span className="white-text">
+                <PreWakeUpTime />
+              </span>
+            </div>
           </div>
-                <div className="col l4 s12">
-                <PomodoroTimer/>
+
+          <div className="col l4 s12">
+            <PomodoroTimer />
           </div>
         </div>
 
         <div className="livedaze_pre_headline">
           View #2 : After User Adds value
         </div>
+
         <div className="row">
           <div className="col col l4 s12 ">
             <div className="card-panel livedazeGrey">
-              <span className="white-text">
-                <div className="livedaze_TabTitle">My D-day</div>
-                <div className="livedaze_daysRemaining">
-                  {remainingDays}
-                  <span className="daysLeft">
-                    days
-                    <br />
-                  </span>
-                </div>
-                <div className="goalDate">{goalFormatted}</div>
-                <div>
-                  It’s been {durationSince}
-                  days since you started.
-                </div>
-              </span>
+              <span className="white-text" />
             </div>
           </div>
           <div className="col l4 s12">
             <div className="card-panel livedazeGrey">
               <span className="white-text">
-                <SinceWakeUp />
+                <SetGoalDateView />
               </span>
             </div>
           </div>
-          <div className="col l4 s12">
-            <div className="card-panel livedazeGrey">
-              <span className="white-text">
-                <div className="card-panel livedazeGrey">
-                  <span className="white-text">
-                    <span className="livedazeGreen">
-                      Hey mark, how’s your progress?
-                    </span>
-                    <form action="#">
-                      <p>
-                        <label>
-                          <input name="group1" type="radio" />
-                          <span>Great. i am very focused and productive.</span>
-                        </label>
-                      </p>
-                      <p>
-                        <label>
-                          <input name="group1" type="radio" />
-                          <span>
-                            IT’s FINE. I AM FOCUSING BUT I AM NOT REALLY SURE.
-                          </span>
-                        </label>
-                      </p>
-                      <p>
-                        <label>
-                          <input name="group1" type="radio" />
-                          <span>I hate doing this. literally.</span>
-                        </label>
-                      </p>
-                      <p>
-                        <label>
-                          <input name="group1" type="radio" />
-                          <span>
-                            YOLO! I DIDN’T DO ANYTHING. I WAS JUST WATCHING
-                            YOUTUBE.
-                          </span>
-                        </label>
-                      </p>
-                    </form>
 
-                    <button
-                      className="btn waves-effect waves-light"
-                      type="submit"
-                      name="action"
-                    >
-                      Submit
-                    </button>
-                  </span>
-                </div>
-              </span>
+          <div className="col l4 s12">
+            <div className="card-panel livedazeGrey">
+              <span className="white-text" />
             </div>
           </div>
         </div>
 
-        <h4>----------------------------------</h4>
-        <h4>Hi {user.firstName}!</h4>
-        <p>Welcome to livedaze.</p>
-
-        <h3>All registered users:</h3>
-        {users.loading && <em>Loading users...</em>}
-        {users.error && (
-          <span className="text-danger">ERROR: {users.error}</span>
-        )}
-        {users.items && (
-          <ul>
-            {users.items.map((user, index) => (
-              <li key={user.id}>
-                {user.firstName + " " + user.lastName}
-                {user.deleting ? (
-                  <em>- Deleting...</em>
-                ) : user.deleteError ? (
-                  <span className="text-danger">
-                    - ERROR: {user.deleteError}
-                  </span>
-                ) : (
-                  <span>
-                    -<a onClick={this.handleDeleteUser(user.id)}>Delete</a>
-                  </span>
-                )}
-              </li>
-            ))}
-          </ul>
-        )}
-        <p>
-          <Link to="/login">Logout</Link>
-        </p>
         <script type="text/javascript" src="js/materialize.min.js" />
       </div>
     );
