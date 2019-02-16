a7134141ae0315d16e840c62a9276eec84ad9687
Mark Kang <kangm210@gmail.com>
2019-02-15 18:46:45 -0800

    Deleting Redundant files

--- src/_components/GoalDate/AfterSetGoalDate.js
+++ src/_components/GoalDate/AfterSetGoalDate.js
@@ -2,35 +2,32 @@ import React, { Component } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-
 export class AfterSetGoalDate extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      goalDate : props.newDate,
+      goalDate: props.newDate,
       startDate: new Date()
     };
   }
-
 
   render() {
     // today
     const date = new Date();
     const goalDate = this.state.goalDate;
 
-
     const startDay = "2018-10-10";
     const moment = require("moment");
 
     const momentizedGoalDate = moment(goalDate).format("LL");
-    const remainingDays = Math.abs(moment(date, "days").diff(momentizedGoalDate, "days")) + 1;
+    const remainingDays =
+      Math.abs(moment(date, "days").diff(momentizedGoalDate, "days")) + 1;
 
     const durationSince =
       Math.abs(moment(date, "days").diff(startDay, "days")) + 1;
 
     return (
-        <React.Fragment>
+      <React.Fragment>
         <div className="livedaze_TabTitle">My D-day</div>
         <div className="livedaze_daysRemaining">
           {remainingDays}
@@ -40,7 +37,7 @@ export class AfterSetGoalDate extends React.Component {
           </span>
         </div>
         <div className="goalDate">{momentizedGoalDate}</div>
-        </React.Fragment>
+      </React.Fragment>
     );
   }
 }
