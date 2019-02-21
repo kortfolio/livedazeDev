3b5fbf4ed66817a6a8e6f5c08aec74a2ae4fbeb3
Mark Kang <kangm210@gmail.com>
2019-02-20 21:25:04 -0800

    Login Pge UI Update In Progress

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,62 +1,56 @@
-import React from 'react';
-import { Router, Route } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React from "react";
+import { Router, Route } from "react-router-dom";
+import { connect } from "react-redux";
 
-import { history } from '../_helpers';
-import { alertActions } from '../_actions';
-import { PrivateRoute } from '../_components';
-import { HomePage } from '../HomePage';
-import { LoginPage } from '../LoginPage';
-import { RegisterPage } from '../RegisterPage';
-import Clock from 'react-live-clock';
+import { history } from "../_helpers";
+import { alertActions } from "../_actions";
+import { PrivateRoute } from "../_components";
+import { HomePage } from "../HomePage";
+import { LoginPage } from "../LoginPage";
+import { RegisterPage } from "../RegisterPage";
+import Clock from "react-live-clock";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        const { dispatch } = this.props;
-        history.listen((location, action) => {
-            // clear alert on location change
-            dispatch(alertActions.clear());
-        });
-    }
+    const { dispatch } = this.props;
+    history.listen((location, action) => {
+      // clear alert on location change
+      dispatch(alertActions.clear());
+    });
+  }
 
-    render() {
-        const { alert } = this.props;
-        let time = new Date().toLocaleString();
+  render() {
+    const { alert } = this.props;
+    let time = new Date().toLocaleString();
 
+    const dateToFormat = "1976-04-19T12:59-0500";
 
-        const dateToFormat = '1976-04-19T12:59-0500';
-
-        return (
-
+    return (
+      <div>
+        <div className="row">
+          {alert.message && (
+            <div className={`alert ${alert.type}`}>{alert.message}</div>
+          )}
+          <Router history={history}>
             <div>
-
-                <div className="row">
-
-                    {alert.message &&
-                            <div className={`alert ${alert.type}`}>{alert.message}</div>
-                        }
-                        <Router history={history}>
-                            <div>
-                                <PrivateRoute exact path="/" component={HomePage} />
-                                <Route path="/login" component={LoginPage} />
-                                <Route path="/register" component={RegisterPage} />
-                            </div>
-                        </Router>
-                    </div>
-
-                </div>
-
-        );
-    }
+              <PrivateRoute exact path="/" component={HomePage} />
+              <Route path="/login" component={LoginPage} />
+              <Route path="/register" component={RegisterPage} />
+            </div>
+          </Router>
+        </div>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    const { alert } = state;
-    return {
-        alert
-    };
+  const { alert } = state;
+  return {
+    alert
+  };
 }
 
 const connectedApp = connect(mapStateToProps)(App);
--- src/LoginPage/LoginPage.jsx
+++ src/LoginPage/LoginPage.jsx
@@ -1,84 +1,95 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import {connect} from 'react-redux';
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-import {userActions} from '../_actions';
+import { userActions } from "../_actions";
 
 class LoginPage extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        // reset login status
-        this.props.dispatch(userActions.logout());
+    // reset login status
+    this.props.dispatch(userActions.logout());
 
-        this.state = {
-            username: '',
-            password: '',
-            submitted: false
-        };
+    this.state = {
+      username: "",
+      password: "",
+      submitted: false
+    };
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    this.setState({ submitted: true });
+    const { username, password } = this.state;
+    const { dispatch } = this.props;
+    if (username && password) {
+      dispatch(userActions.login(username, password));
     }
+  }
 
-    handleChange(e) {
-        const {name, value} = e.target;
-        this.setState({[name]: value});
-    }
+  render() {
+    const { loggingIn } = this.props;
+    const { username, password, submitted } = this.state;
+    return (
+      <div className="row">
+        <div className="col s6">
+        <h5 class="center-align">
+          <img
+            className="responsive-img center"
+            src="../images/welcome.png"
+            width="300px"
+          />
+          </h5>
+          <div className="livedazeMissionStatement center">
+            Focus on Today. Just Today
+          </div>
+          <div className="livedazeSubMissionStatement">
+            Get things done to reach your goal
+          </div>
 
-    handleSubmit(e) {
-        e.preventDefault();
+          {/* LiveDaze Mission Statement Descriptions */}
 
-        this.setState({submitted: true});
-        const {username, password} = this.state;
-        const {dispatch} = this.props;
-        if (username && password) {
-            dispatch(userActions.login(username, password));
-        }
-    }
+          <ul className="liveDazeMsDetails">
+            <li>
+              •   Get to know yourself.
+              <ul>
+                <li className="subDescriptionMS">
+                Figure out what you are good at and what you are not.
+                </li>
+              </ul>
+            </li>
 
-    render() {
-
-        const {loggingIn} = this.props;
-        const {username, password, submitted} = this.state;
-        return (
-            <div className="row">
-                <div className="col s6">
-                    <div className="livedazeMissionStatement">Focus on Today. Just Today</div>
-                    <div className="livedazeSubMissionStatement">Get things done to reach your goal</div>
-
-                    {/* LiveDaze Mission Statement Descriptions */}
-
-                    <ul className="liveDazeMsDetails">
-                        <li>
-                            • Get to know your self first.
-                            <ul>
-                            <li className="subDescriptionMS">
-                                Yes. Simple as that. Using our TimeRefactoring technique. to realize how much time you
-                                can spend to improve/learn in a daily basis.
-                            </li>
-                            </ul>
-                        </li>
-
-                        <li>
-                            • Set your Daily planner.
-                            <ul><li className="subDescriptionMS">
-                            You only got like 5~7 hours day to actually do something for YOUR SELF. Set a realistic goal. daily goal. and DO IT.
-                            </li>
-                            </ul>
-                        </li>
-                        <li>
-                            • result-oriented personal tracker
-                            <ul>
-                            <li className="subDescriptionMS">
-                                If you have no result based on your improvement, it's useless.
-                            </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-                {/* END LiveDaze Mission Statement Descriptions */}
-                {/*
+            <li>
+              • Set your Daily planner.
+              <ul>
+                <li className="subDescriptionMS">
+                  You only got like 5~7 hours day to actually do something for
+                  YOUR SELF. Set a realistic goal. daily goal. and DO IT.
+                </li>
+              </ul>
+            </li>
+            <li>
+              • result-oriented personal tracker
+              <ul>
+                <li className="subDescriptionMS">
+                  If you have no result based on your improvement, it's useless.
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        {/* END LiveDaze Mission Statement Descriptions */}
+        {/*
                 <div className="input-field col s12">
                     <input id="email" type="email" className="validate"/>
                         <label htmlFor="email">Email</label>
@@ -90,35 +101,55 @@ class LoginPage extends React.Component {
                 </div>
                 */}
 
-                <div className="col s6">
-                    <div className="livedazeMissionStatement">Login</div>
-                    <div className="description">Start using Livedaze to keep your day organize.</div>
+        <div className="col s6">
+          <div className="livedazeMissionStatement">Login</div>
+          <div className="description">
+            Start using Livedaze to keep your day organize.
+          </div>
 
-                    <form name="form" onSubmit={this.handleSubmit}>
-                        <div className={'input-field col s12' + (submitted && !username ? ' has-error' : '')}>
-                            <label htmlFor="username">Email</label>
-                            <input type="text" className="validate" name="username" value={username}
-                                   onChange={this.handleChange}/>
-                            {submitted && !username &&
-                            <div className="help-block">Username is required</div>
-                            }
-                        </div>
+          <form name="form" onSubmit={this.handleSubmit}>
+            <div
+              className={
+                "input-field col s12" +
+                (submitted && !username ? " has-error" : "")
+              }
+            >
+              <label htmlFor="username">Email</label>
+              <input
+                type="text"
+                className="validate"
+                name="username"
+                value={username}
+                onChange={this.handleChange}
+              />
+              {submitted && !username && (
+                <div className="help-block">Username is required</div>
+              )}
+            </div>
 
+            <div
+              className={
+                "input-field col s12" +
+                (submitted && !password ? " has-error" : "")
+              }
+            >
+              <label htmlFor="password">Enter your password</label>
 
+              <input
+                type="password"
+                className="validate"
+                name="password"
+                value={password}
+                id="password"
+                onChange={this.handleChange}
+              />
 
-                        <div className={'input-field col s12' + (submitted && !password ? ' has-error' : '')}>
-                            <label htmlFor="password">Enter your password</label>
+              {submitted && !password && (
+                <div className="help-block">Password is required</div>
+              )}
+            </div>
 
-                            <input type="password" className="validate" name="password" value={password} id="password"
-                                   onChange={this.handleChange}/>
-
-                            {submitted && !password &&
-                            <div className="help-block">Password is required</div>
-                            }
-                        </div>
-
-
-                        {/* Original Source Code
+            {/* Original Source Code
                         <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
                             <label htmlFor="password">Password</label>
                             <input type="password" className="form-control" name="password" value={password} id="password"
@@ -129,35 +160,35 @@ class LoginPage extends React.Component {
                         </div>
                         */}
 
-                        <div className="form-group">
-                            <button className="btn btn-primary">Login</button>
-                            {loggingIn &&
-                            <img
-                                src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA=="/>
-                            }
-                            <Link to="/register" className="btn btn-link">Register</Link>
-                        </div>
-                    </form>
-                    <button className="loginBtn loginBtn--facebook">
-                        Login with Facebook
-                    </button>
-
-                    <button className="loginBtn loginBtn--google">
-                        Login with Google
-                    </button>
-                </div>
+            <div className="form-group">
+              <button className="btn btn-primary">Login</button>
+              {loggingIn && (
+                <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+              )}
+              <Link to="/register" className="btn btn-link">
+                Register
+              </Link>
             </div>
+          </form>
+          <button className="loginBtn loginBtn--facebook">
+            Login with Facebook
+          </button>
 
-        );
-    }
+          <button className="loginBtn loginBtn--google">
+            Login with Google
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    const {loggingIn} = state.authentication;
-    return {
-        loggingIn
-    };
+  const { loggingIn } = state.authentication;
+  return {
+    loggingIn
+  };
 }
 
 const connectedLoginPage = connect(mapStateToProps)(LoginPage);
-export {connectedLoginPage as LoginPage};
+export { connectedLoginPage as LoginPage };
